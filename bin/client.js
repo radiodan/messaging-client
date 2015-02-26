503e5b8d0694f55e78dd2a6b2dfb80d6278631eb
Dan Nuttall <pixelblend@gmail.com>
2015-02-26 17:47:11 +0000

    Use promises to feedback command response

--- bin/client.js
+++ bin/client.js
@@ -3,4 +3,8 @@
 var Client = require('../lib/messaging-client').Client,
     client = Client.create('tcp://127.0.0.1:7171');
 
-client.sendCommand('player', 'avoider', 'play', {position: 71});
+client.sendCommand('player', 'avoider', 'play', {position: 71})
+  .then(
+    function(response) { console.log('yes!', response) },
+    function(err, response) { console.log('no!', err, response) }
+  );
