f1eba36600928af474072ed047eacc431078bf5c
Dan Nuttall <pixelblend@gmail.com>
2015-02-27 12:46:42 +0000

    Keep player name consistant between client and worker

--- bin/client.js
+++ bin/client.js
@@ -3,7 +3,7 @@
 var Client = require('../lib/messaging-client').Client,
     client = Client.create('tcp://127.0.0.1:7171');
 
-client.sendCommand('player', 'avoider', 'play', {position: 71})
+client.sendCommand('player', 'avoid', 'play', {position: 71})
   .then(
     function(response) { console.log('yes!', response) },
     function(err, response) { console.log('no!', err, response) }
