21d93fea7a0bf50253efb8f6a9dad82b09ae2250
Dan Nuttall <pixelblend@gmail.com>
2015-03-01 11:35:00 +0000

    Add repeater to client executable

--- bin/client.js
+++ bin/client.js
@@ -1,10 +1,36 @@
 #!/usr/bin/env node
 
 var Client = require('../lib/messaging-client').Client,
-    client = Client.create('tcp://127.0.0.1:7171');
+    client = Client.create('tcp://127.0.0.1:7171'),
+    loop   = parseInt(process.env.LOOP) || 1,
+    Q = require('q'),
+    promises = [];
 
-client.sendCommand('player', 'avoid', 'play', {position: 71})
-  .then(
-    function(response) { console.log('yes!', response) },
-    function(err, response) { console.log('no!', err, response) }
-  );
+function sendCommand(count) {
+  console.log(count, "<-")
+  var p = client.sendCommand('player', 'avoid', 'play', {position: 71})
+    .then(
+      function(response) { console.log(count, '-> yes!', response) },
+      function(err, response) { console.log(count, '-> no!', err, response) }
+    );
+
+  promises.push(p);
+  if(promises.length == loop) {
+    Q.all(promises).then(function() {
+      console.timeEnd(loop);
+    });
+  }
+}
+
+var i =1;
+
+function exec() {
+  sendCommand(i++);
+
+  if(i < loop+1) {
+    setTimeout(exec, 0);
+  }
+}
+
+console.time(loop);
+exec()
