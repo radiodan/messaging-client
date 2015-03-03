b3df01336eaf7b03a269cef9b4143849cf3a805d
Dan Nuttall <pixelblend@gmail.com>
2015-03-03 14:26:31 +0000

    Add new service setting to worker script

--- bin/worker.js
+++ bin/worker.js
@@ -3,5 +3,9 @@
 var Worker = require('../lib/messaging-client').Worker,
     worker = Worker.create('tcp://127.0.0.1:7171', 'radiodan-player');
 
-worker.addService('player', ['announce', 'avoid']);
+worker.addService([
+  {serviceType: 'player', serviceInstances: ['announce', 'avoid']},
+  {serviceType: 'player', serviceInstances: ['main']},
+  {serviceType: 'button', serviceInstances: ['power', 'nextstation']}
+]);
 worker.ready();
